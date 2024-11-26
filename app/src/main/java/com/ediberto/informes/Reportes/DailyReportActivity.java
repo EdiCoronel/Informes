d9344c4799d83e66d073ebf6006ff54cd21de905
Ediberto <coronelvediberto@gmail.com>
2024-11-26 04:04:49 -0300

    Colocando boton para cargar imagen en informe

--- app/src/main/java/com/ediberto/informes/Reportes/DailyReportActivity.java
+++ app/src/main/java/com/ediberto/informes/Reportes/DailyReportActivity.java
@@ -1,24 +1,30 @@
 package com.ediberto.informes.Reportes;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.provider.MediaStore;
 import com.ediberto.informes.BasedeDatos.DailyReportDatabaseHelper;
 import com.ediberto.informes.R;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class DailyReportActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PICK_IMAGE = 2;
 
     private TextView dateTextView;
     private EditText locationEditText;
@@ -28,6 +34,8 @@ public class DailyReportActivity extends AppCompatActivity {
     private EditText endTimeEditText;
     private Button submitButton;
     private Button viewReportButton;
+    private Button buttonSelectPhoto;
+    private ImageView imageView;
     private DailyReportDatabaseHelper dbHelper; // Nueva instancia de la base de datos
 
     @Override
@@ -49,6 +57,9 @@ public class DailyReportActivity extends AppCompatActivity {
         endTimeEditText = findViewById(R.id.endTimeEditText);
         submitButton = findViewById(R.id.submitButton);
         viewReportButton = findViewById(R.id.viewReportButton);
+        buttonSelectPhoto = findViewById(R.id.buttonSelectPhoto);
+        imageView = findViewById(R.id.imageView);
+
         dbHelper = new DailyReportDatabaseHelper(this);
 
         // Establecer la fecha actual
@@ -74,6 +85,7 @@ public class DailyReportActivity extends AppCompatActivity {
                 observationsEditText.setText("");
                 startTimeEditText.setText("");
                 endTimeEditText.setText("");
+                imageView.setImageDrawable(null);
             }
         });
 
@@ -81,12 +93,35 @@ public class DailyReportActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Asume que tienes el ID del informe que deseas ver
-                int reportId = 1; // Cambia esto según la lógica de tu aplicación
+                int reportId = 1;
                 Intent intent = new Intent(DailyReportActivity.this, ViewReportActivity.class);
                 intent.putExtra("REPORT_ID", reportId); // Pasar el ID del informe
                 startActivity(intent);
             }
         });
+
+        buttonSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhotoIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhotoIntent, PICK_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageView.setImageBitmap(imageBitmap);
+            } else if (requestCode == PICK_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                imageView.setImageURI(selectedImageUri);
+            }
+        }
     }
 
     @Override
